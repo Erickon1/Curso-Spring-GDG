09e9171a6d226f2de55cd97a6e2bb2a0bb909d44
Erick Roberto Mercado Arroyo <erick.mercado@dinerio.mx>
2017-11-18 23:08:42 -0600

    backend

---
+++ src/main/groovy/com/example/CursoGDG/CursoGdgApplication.groovy
@@ -0,0 +1,12 @@
+package com.example.CursoGDG
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CursoGdgApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run CursoGdgApplication, args
+	}
+}
